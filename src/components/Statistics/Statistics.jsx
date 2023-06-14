967d85affef2d6a6a1f73a84b1c466d67c698c0d
Franklinshtein <98875248+Franklinshtein@users.noreply.github.com>
2023-06-14 14:58:32 +0200

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,7 +6,7 @@ let getRandomHexColor = function() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
     
-  export const Statistics = ({title,statData }) => {
+  export const Statistics = ({statData }) => {
    
    return (
      <section className={styles.statistics}>
